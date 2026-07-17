f2be53adbd07c03aa73eed0e420f1838d9404eb6
Ivan Tran <mystoganx2001@gmail.com>
2026-07-17 13:32:37 +1000

    fix: clean seed to work with dev

--- prisma/seed-clean.ts
+++ prisma/seed-clean.ts
@@ -1,5 +1,7 @@
 import dotenv from "dotenv";
+
 dotenv.config({ path: ".env", quiet: true });
+dotenv.config({ path: ".env.local", override: true, quiet: true });
 
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,7 @@
 import dotenv from "dotenv";
+
 dotenv.config({ path: ".env", quiet: true });
+dotenv.config({ path: ".env.local", override: true, quiet: true });
 
 import {
   PrismaClient,
